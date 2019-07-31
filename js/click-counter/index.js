d5af878a672541d4b03b073f7d90741d24765656
Shishmarev Ilya <shishmarevi@inbox.ru>
2019-07-31 23:27:46 +0300

    click-counter

--- js/click-counter/index.js
+++ js/click-counter/index.js
@@ -1,3 +1,36 @@
+class Panel {
+  /**
+   * @type null | HTMLDivElement
+   */
+  element = null;
+  counter = 0;
+
+  get counterElement() {
+    const className = this.element.className;
+
+    return document.querySelector(`.${className}-counter`);
+  }
+
+  init(el) {
+    if (!el) return false;
+
+    this.element = el;
+    this.element.addEventListener('click', this.handleClick);
+  }
+
+  destroy() {
+    if (!this.element) return false;
+
+    this.element.removeEventListener('click', this.handleClick);
+  }
+
+  handleClick = () => {
+    if (!this.counterElement) return false;
+
+    this.counterElement.innerHTML = `${++this.counter}`;
+  }
+}
+
 function main () {
   var panelsElements = document.querySelector('.wrapper');
   [].slice.apply(panelsElements.children).forEach(function (panelElement) {
