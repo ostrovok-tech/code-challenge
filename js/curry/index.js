1083aa2e3e539a5385bb31b0ed1ab896d1a534d2
Misha Pryadun <pryadun99@icloud.com>
2021-07-17 19:46:12 +0500

    Sloving the task "curry"

--- js/curry/index.js
+++ js/curry/index.js
@@ -1,10 +1,28 @@
+let curry = function (fn) {
+    let arity = fn.length;
+    return function f1(...args){
+        if (args.length >= arity){
+            return fn (...args)
+        } else{
+            return function f2(...moreArgs) {
+                let newArgs = args.concat(moreArgs)
+                return f1(...newArgs)
+            }
+        }
+    }
+}
+
 function abc(a, b, c) {
   return a + b + c;
 }
 
+abc.curry = curry(abc);
+
 function abcdef(a, b, c, d, e, f) {
   return a + b + c + d + e + f;
 }
+
+abcdef.curry = curry(abcdef);
 
 function assert (a, b) {
   if (a !== b) {
